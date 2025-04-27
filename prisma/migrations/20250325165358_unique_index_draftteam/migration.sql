6c31113b65ccf582f3340879660643de6879ae17
stubob <stewartj76@gmail.com>
2025-04-26 21:54:01 -0500

    local setup

--- prisma/migrations/20250325165358_unique_index_draftteam/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[rider_id,user_id,team_id]` on the table `draft_team_riders` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "draft_team_riders_rider_id_user_id_team_id_key" ON "draft_team_riders"("rider_id", "user_id", "team_id");
